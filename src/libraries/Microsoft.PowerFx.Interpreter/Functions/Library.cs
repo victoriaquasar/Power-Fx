7c6de0ba3597552ce927907d80c99a397f071aa2
Anh Truong <anhtruong@microsoft.com>
2022-08-31 11:56:14 -0700

    Add missing paranthesis

--- src/libraries/Microsoft.PowerFx.Interpreter/Functions/Library.cs
+++ src/libraries/Microsoft.PowerFx.Interpreter/Functions/Library.cs
@@ -469,7 +469,7 @@ namespace Microsoft.PowerFx.Functions
                     checkRuntimeTypes: ExactValueTypeOrBlank<NumberValue>,
                     checkRuntimeValues: DeferRuntimeValueChecking,
                     returnBehavior: ReturnBehavior.AlwaysEvaluateAndReturnResult,
-                    targetFunction: Dec2Hex
+                    targetFunction: Dec2Hex)
             },
             {
                 BuiltinFunctionsCore.EndsWith,
@@ -652,7 +652,7 @@ namespace Microsoft.PowerFx.Functions
                     checkRuntimeTypes: ExactValueTypeOrBlank<StringValue>,
                     checkRuntimeValues: DeferRuntimeValueChecking,
                     returnBehavior: ReturnBehavior.AlwaysEvaluateAndReturnResult,
-                    targetFunction: Hex2Dec
+                    targetFunction: Hex2Dec)
             },
             {
                 BuiltinFunctionsCore.Hour,
